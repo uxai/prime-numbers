e5323c71524585e70bb86a6d42dafd3161b8063a
Matthew <matt.talebi@live.com>
2023-08-01 16:03:42 -0700

    Update primes.py
    Updated to allow user input

--- primes.py
+++ primes.py
@@ -1,13 +1,17 @@
+import math
+
 def check_prime(num):
     for n in range(2,int(num**0.5)+1):
         if num % n == 0:
             return False
     return print(r, end="\t")
 
-keys = (1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 
-        47, 49, 53, 59, 61, 67, 71, 73, 77, 79, 83, 89)
+prime_limit = int(input("To what range would you like to check the primes?"))
+prime_limit = math.floor(prime_limit / 90)
 
-for n in range(0, 100):
+keys = (1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59, 61, 67, 71, 73, 77, 79, 83, 89)
+
+for n in range(0, prime_limit):
     for i in range(0, len(keys)):
         r = 90 * n + keys[i]
         if r == 1:
